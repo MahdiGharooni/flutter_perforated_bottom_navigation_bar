e45c7d64379038c9af3fede6a2b2edbcb860eb30
MahdiGharooni <39171814+MahdiGharooni@users.noreply.github.com>
2020-09-29 16:18:53 +0330

    Update perforated_bottom_navigation_bar.dart

--- lib/perforated_bottom_navigation_bar.dart
+++ lib/perforated_bottom_navigation_bar.dart
@@ -1,19 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class PerforatedBottomNavigationBar extends StatefulWidget {
-  static const MethodChannel _channel =
-      const MethodChannel('perforated_bottom_navigation_bar');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
 
   /// scaffold body
-  final Widget body;
+  final List<Widget> body;
 
   /// scaffold drawer
   final Widget drawer;
@@ -36,43 +26,65 @@ class PerforatedBottomNavigationBar extends StatefulWidget {
   /// icons tooltips
   final List<String> iconsTooltips;
 
-  /// first of all this index selected for bottomNavigationBar
+  /// first of all this index selected for buttonNavigationBar
   final int initialIndex;
 
-  /// bottom navigation bar elevation
-  final double bottomBarElevation;
+  /// button navigation bar elevation
+  final double buttonBarElevation;
 
-  /// color of icon except main big bottom
+  /// color of icon except main big button
   final Color iconColor;
 
-  /// color of main bottom icon
-  final Color bottomIconColor;
+  /// color of icon when selected
+  final Color selectedIconColor;
 
-  /// size of main bottom icon
-  final double bottomIconSize;
+  /// color of main button icon
+  final Color buttonIconColor;
 
-  /// bottom bare key
+  /// size of main button icon
+  final double buttonIconSize;
+
+  /// button bare key
   final Key bottomBarKey;
 
-  /// bottom bar color
+  /// button bar color
   final Color bottomBarColor;
+
+  /// tabs titles
+  final List<Widget> appBarTitles;
+
+  /// app bar elevation
+  final List<Widget> appBarElevation;
+
+  /// appbar background color
+  final Color appBarBackgroundColor;
+
+  /// appbar center title
+  final bool appBarCenterTitle;
+
+  /// button background color
+  final Color buttonBackgroundColor ;
 
   PerforatedBottomNavigationBar({
     @required this.body,
     @required this.icons,
     @required this.initialIndex,
-    this.appBar,
     this.drawer,
     this.scaffoldKey,
     this.selectedIconSize,
     this.unSelectedIconSize,
     this.iconsTooltips,
-    this.bottomBarElevation,
+    this.buttonBarElevation,
     this.iconColor,
-    this.bottomIconColor,
-    this.bottomIconSize,
+    this.buttonIconColor,
+    this.buttonIconSize,
     this.bottomBarKey,
     this.bottomBarColor,
+    this.appBarTitles,
+    this.appBar,
+    this.appBarElevation,
+    this.appBarBackgroundColor,
+    this.appBarCenterTitle, this.selectedIconColor, this.buttonBackgroundColor,
   });
 
   @override
@@ -94,18 +106,25 @@ class _PerforatedBottomNavigationBarState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: widget.appBar ?? Container(),
-      drawer: widget.drawer ?? Container(),
-      body: widget.appBar ?? Container(),
+      appBar: widget.appBar ??
+          AppBar(
+            title: widget.appBarTitles[_index],
+            elevation: widget.appBarElevation ?? 0,
+            backgroundColor: widget.appBarBackgroundColor ?? Colors.transparent,
+            centerTitle: widget.appBarCenterTitle ?? true,
+          ),
+      drawer: widget.drawer ?? null,
+      body: widget.body[_index] ?? Container(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _tabSelected(2),
-        tooltip: widget.iconsTooltips[2],
+        tooltip: widget.iconsTooltips[2] ?? '',
         child: Icon(
           widget.icons[2],
-          color: widget.bottomIconColor,
-          size: widget.bottomIconSize ?? 35,
+          color: widget.buttonIconColor,
+          size: widget.buttonIconSize ?? 35,
         ),
-        elevation: widget.bottomBarElevation ?? 2.0,
+        elevation: widget.buttonBarElevation ?? 2.0,
+        backgroundColor: widget.buttonBackgroundColor??null,
       ),
       bottomNavigationBar: BottomAppBar(
         child: Container(
@@ -115,11 +134,11 @@ class _PerforatedBottomNavigationBarState
               IconButton(
                 icon: Icon(
                   widget.icons[4],
-                  color: widget.iconColor,
+                  color: (widget.selectedIconColor != null && _index == 4)?widget.selectedIconColor  : widget.iconColor,
                 ),
                 onPressed: () => _tabSelected(4),
                 padding: EdgeInsets.all(0.0),
-                tooltip: widget.iconsTooltips[4],
+                tooltip: widget.iconsTooltips[4] ?? '',
                 iconSize: _index == 4
                     ? (widget.selectedIconSize ?? 30)
                     : (widget.unSelectedIconSize ?? 25),
@@ -127,11 +146,11 @@ class _PerforatedBottomNavigationBarState
               IconButton(
                 icon: Icon(
                   widget.icons[3],
-                  color: widget.iconColor,
+                  color: (widget.selectedIconColor != null && _index == 3)?widget.selectedIconColor  : widget.iconColor,
                 ),
                 onPressed: () => _tabSelected(3),
                 padding: EdgeInsets.all(0.0),
-                tooltip: widget.iconsTooltips[3],
+                tooltip: widget.iconsTooltips[3] ?? '',
                 iconSize: _index == 3
                     ? (widget.selectedIconSize ?? 30)
                     : (widget.unSelectedIconSize ?? 25),
@@ -142,11 +161,11 @@ class _PerforatedBottomNavigationBarState
               IconButton(
                 icon: Icon(
                   widget.icons[1],
-                  color: widget.iconColor,
+                  color: (widget.selectedIconColor != null && _index == 1)?widget.selectedIconColor  : widget.iconColor,
                 ),
                 onPressed: () => _tabSelected(1),
                 padding: EdgeInsets.all(0.0),
-                tooltip: widget.iconsTooltips[1],
+                tooltip: widget.iconsTooltips[1] ?? '',
                 iconSize: _index == 1
                     ? (widget.selectedIconSize ?? 30)
                     : (widget.unSelectedIconSize ?? 25),
@@ -154,11 +173,11 @@ class _PerforatedBottomNavigationBarState
               IconButton(
                 icon: Icon(
                   widget.icons[0],
-                  color: widget.iconColor,
+                  color: (widget.selectedIconColor != null && _index == 0)?widget.selectedIconColor  : widget.iconColor,
                 ),
                 onPressed: () => _tabSelected(0),
                 padding: EdgeInsets.all(0.0),
-                tooltip: widget.iconsTooltips[0],
+                tooltip: widget.iconsTooltips[0] ?? '',
                 iconSize: _index == 0
                     ? (widget.selectedIconSize ?? 30)
                     : (widget.unSelectedIconSize ?? 25),
@@ -167,9 +186,9 @@ class _PerforatedBottomNavigationBarState
           ),
         ),
         shape: CircularNotchedRectangle(),
-        elevation: widget.bottomBarElevation ?? 15.0,
+        elevation: widget.buttonBarElevation ?? 15.0,
         color: widget.bottomBarColor,
-        key: widget.bottomBarKey ?? Key('bottomNavigationBarKey'),
+        key: widget.bottomBarKey ?? Key('buttonNavigationBarKey'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       key: widget.scaffoldKey ?? Key('PerforatedBottomNavigationBarKey'),
